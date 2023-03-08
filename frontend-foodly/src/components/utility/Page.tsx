45ca3b4ae4f046faa16cce8ed46f73da6731972e
Aziz Bektemirov <bektemirovaziz64@yandex.com>
2023-03-08 17:49:29 +0500

    fix error from Next

---
+++ frontend-foodly/src/components/utility/Page.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import type { FC, ReactNode } from 'react';
+import { Fragment } from 'react';
+
+export const Page: FC<PageProps> = ({ children, title }) => {
+    const pageTitle = `${title} - Foodly`;
+
+    return (
+        <Fragment>
+            <Head>
+                <title>{pageTitle}</title>
+            </Head>
+            {children}
+        </Fragment>
+    );
+};
+
+interface PageProps {
+    children: ReactNode;
+    title: string;
+}
