932a1f22bd918a93c6481214d46fe897e8af8427
Aziz Bektemirov <bektemirovaziz64@yandex.com>
2023-04-18 14:20:36 +0500

    DELIVERY: adds style configs for the bottom tabs navigation

--- delivery-foodly/src/screens/_router.tsx
+++ delivery-foodly/src/screens/_router.tsx
@@ -8,19 +8,74 @@ import { LoginScreen } from './Login.screen';
 import type { RootStackParamList, TabParamList } from './types';
 import { useAuth } from '../contexts/auth';
 import { Fragment } from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Colors } from '../styles/colors';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 const Tab = createBottomTabNavigator<TabParamList>();
 const TabsNavigator = () => {
+    const { bottom } = useSafeAreaInsets();
+
+    const iconSize = 25;
+
     return (
         <Tab.Navigator
             screenOptions={{
                 headerShown: false,
+                tabBarStyle: {
+                    height: 60 + bottom,
+                    backgroundColor: Colors.amber[600],
+                    borderTopLeftRadius: 25,
+                    borderTopRightRadius: 25,
+                },
+                tabBarLabelStyle: {
+                    marginTop: Platform.OS === 'android' ? -5 : -7,
+                    fontSize: 16,
+                },
+                tabBarInactiveTintColor: Colors.gray[300],
+                tabBarActiveTintColor: Colors.white,
             }}
         >
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Deliveries" component={DeliveriesScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon({ color }) {
+                        return <MaterialIcons
+                            name="home-filled"
+                            size={iconSize}
+                            color={color}
+                        />;
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="Deliveries"
+                component={DeliveriesScreen}
+                options={{
+                    tabBarIcon({ color }) {
+                        return <MaterialIcons
+                            name="local-shipping"
+                            size={iconSize}
+                            color={color}
+                        />;
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon({ color }) {
+                        return <MaterialIcons
+                            name="person"
+                            size={iconSize}
+                            color={color}
+                        />;
+                    }
+                }}
+            />
         </Tab.Navigator>
     );
 };
